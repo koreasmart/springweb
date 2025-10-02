adf3f38a724c5836debda24226f3811f4a807cc1
koreasmart <coolhongs@naver.com>
2025-10-02 10:46:50 +0900

    first commit

---
+++ springweb/src/test/java/ksmart/springweb/SpringwebApplicationTests.java
@@ -0,0 +1,13 @@
+package ksmart.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
